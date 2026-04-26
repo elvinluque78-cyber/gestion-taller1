434e27c0373eb469802520395b5a117c6a47029c
Elvin Luque <elvinluque78@gmail.com>
2026-04-26 15:17:42 -0400

    Forzar puerto 8080

--- app.py
+++ app.py
@@ -227,5 +227,5 @@ if __name__ == "__main__":
     if not os.path.exists(DB_NAME):
         import db
         db.init_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
